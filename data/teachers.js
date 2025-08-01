9bda125f947bcf09e5bdb73e7bba3ae9992e0059
Kojo Amfo-Baah <Kojo8baah@gmail.com>
2025-08-01 15:41:13 +0000

    Final-update-4

--- data/teachers.js
+++ data/teachers.js
@@ -14,6 +14,42 @@ export const teachers = [
     subjects: ["Science", "Chemistry"],
     avatar: ProfileImage2
   },
+  {
+    name: "Jeanette Horse",
+    email: "j.horse@teacher.io",
+    subjects: ["Chemistry", "Science"],
+    avatar: ProfileImage1
+  },
+  {
+    name: "Josh Homme",
+    email: "j.homme@teacher.io",
+    subjects: ["Mathematics"],
+    avatar: ProfileImage2
+  },
+  {
+    name: "Jeanette Horse",
+    email: "j.horse@teacher.io",
+    subjects: ["Literature"],
+    avatar: ProfileImage1
+  },
+  {
+    name: "Josh Homme",
+    email: "j.homme@teacher.io",
+    subjects: ["Art", "Chemistry"],
+    avatar: ProfileImage2
+  },
+  {
+    name: "Jeanette Horse",
+    email: "j.horse@teacher.io",
+    subjects: ["Art", "History"],
+    avatar: ProfileImage1
+  },
+  {
+    name: "Josh Homme",
+    email: "j.homme@teacher.io",
+    subjects: ["Science", "Chemistry"],
+    avatar: ProfileImage2
+  },
   
   
 ]
